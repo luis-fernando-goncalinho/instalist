9e6ad206f46146a0500c7e380ae81ce0db52d7a8
Luis Goncalinho <lfergon191@gmail.com>
2023-05-28 17:03:26 -0300

    adicionando ícone de selecionado após selecao de mídia

--- app/javascript/controllers/selector_controller.js
+++ app/javascript/controllers/selector_controller.js
@@ -1,12 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+
+  static targets = ["media", "icon"]
+
   connect() {
     //this.element.textContent = "Hello World!";
     console.log('connected')
   }
 
   select() {
-    this.element.classList.toggle("media-selected");
+    this.mediaTarget.classList.toggle("media-selected");
+    this.iconTarget.classList.toggle("d-none");
   }
 }
