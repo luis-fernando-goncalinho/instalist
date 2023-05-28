554605d65a0fdc09398b26acbab53e54f9b642f4
arthur finamore <arthurfinamore@gmail.com>
2023-05-28 14:29:28 -0300

    added stimulus function to select images

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    //this.element.textContent = "Hello World!";
+    console.log('connected')
+  }
+
+  select() {
+    this.element.classList.toggle("media-selected");
   }
 }
