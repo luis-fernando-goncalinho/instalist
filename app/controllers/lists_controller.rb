ff1232a428c68f66ecdbcced243492b7b849428a
Luis Goncalinho <lfergon191@gmail.com>
2023-05-29 15:14:02 -0300

    acao de envio das postasgens selecionadas para ação de create list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,15 +1,25 @@
 class ListsController < ApplicationController
 
   def index
+    raise
   end
 
   def create
+    @medias = params[:medias]
+    @list_name = params[:list_name]
+
+    respond_to do |format|
+      format.html
+      format.text { render json: { message: params }, status: :created, formats: [:json] }
+    end
   end
 
   def new
+    @list = List.new
+
     user_token = ENV.fetch("INSTAGRAM_USER_TOKEN")
     fields = "media_url,media_type,caption,permalink,timestamp,thumbnail_url"
-    limit = "10"
+    limit = "12"
 
     url = "https://graph.instagram.com/me/media?access_token=#{user_token}&fields=#{fields}&limit=#{limit}"
     response = HTTParty.get(url)
@@ -28,5 +38,11 @@ class ListsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def permitted_params
+    params.permit!
   end
 end
--- app/javascript/controllers/media_list_controller.js
+++ app/javascript/controllers/media_list_controller.js
@@ -19,6 +19,44 @@ export default class extends Controller {
       selected_medias.push(JSON.parse(element.dataset.media));
     });
 
-    console.dir(selected_medias)
+    return selected_medias
+  }
+
+  createList() {
+
+    const medias = this.getSelectedMedias();
+    const listName = 'Teste';
+    const url = '/lists';
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+    console.dir(medias);
+
+    const data = {
+      medias: medias,
+      list_name: listName
+    };
+
+    console.log(JSON.stringify(data));
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Accept': 'text/plain',
+        'X-CSRF-Token': csrfToken,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+
+    fetch(url, options)
+      .then(response => response.text())
+      .then(data => {
+        // Handle the response from the server
+        console.log(data);
+      })
+      .catch(error => {
+        // Handle any errors that occurred during the request
+        console.error(error);
+      });
+
   }
 }
