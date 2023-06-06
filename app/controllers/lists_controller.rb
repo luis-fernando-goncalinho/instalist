7e40e7ad6582cd7d4929604a38e5b89fda90ada3
Luis Goncalinho <lfergon191@gmail.com>
2023-06-06 23:50:15 +0100

    update

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -49,7 +49,7 @@ class ListsController < ApplicationController
   def edit
     user_token = ENV.fetch("INSTAGRAM_USER_TOKEN")
     fields = "media_url,media_type,caption,permalink,timestamp,thumbnail_url"
-    limit = "24"
+    limit = "100"
 
     url = "https://graph.instagram.com/me/media?access_token=#{user_token}&fields=#{fields}&limit=#{limit}"
     response = HTTParty.get(url)
