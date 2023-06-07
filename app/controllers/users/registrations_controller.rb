3ba1c56834476cb9771644e0ba1a47f8e1c03a19
arthur finamore <arthurfinamore@gmail.com>
2023-06-06 22:35:10 -0300

    added the delete user button, but it does not work

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -43,12 +43,24 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
    devise_parameter_sanitizer.permit(:sign_up, keys: [:image])
-   end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:image])
   end
+
+  def destroy
+    # Carregar o usuário atual
+    @user = current_user
+    @user.destroy
+
+    sign_out @user
+
+    # Redirecionar para a página inicial ou qualquer outra página desejada após a exclusão
+    redirect_to root_path, notice: "Sua conta foi excluída com sucesso."
+  end
+
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
