3d41f6c8e55d1a280a0a0e9f1f84e15346d6053c
arthur finamore <arthurfinamore@gmail.com>
2023-06-15 21:19:50 -0300

    creating favorite post

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,6 @@
 class ListsController < ApplicationController
   before_action :set_list, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:show, :auth]
 
   def index
     @lists = List.all
